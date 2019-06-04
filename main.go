46c7773080c7b797285de585c0f3d0dc715b28e9
banobepascal <banobepascal@gmail.com>
2019-06-04 11:59:51 +0300

    its coming

--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import (
 	"html/template"
 	"net/http"
 
+	"golang.org/x/crypto/bcrypt"
+
 	uuid "github.com/satori/go.uuid"
 )
 
 type user struct {
-	UserName   string
-	Email      string
-	Password   string
-	Repassword string
+	UserName string
+	Email    string
+	Password []byte
 }
 
 var tpl *template.Template
@@ -20,12 +21,16 @@ var dbSessions = map[string]string{}
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
+	bs, _ := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.MinCost)
+	dbUsers["badboy"] = user{"badboy", "pascal@test.com", bs}
+
 }
 
 func main() {
 
 	http.HandleFunc("/userPage", userPage)
 	http.HandleFunc("/", signup)
+	http.HandleFunc("/login", login)
 	http.Handle("/css/", http.FileServer(http.Dir("public")))
 	http.Handle("/img/", http.FileServer(http.Dir("public")))
 	http.Handle("/js/", http.FileServer(http.Dir("public")))
@@ -53,7 +58,6 @@ func signup(w http.ResponseWriter, req *http.Request) {
 		un := req.FormValue("username")
 		e := req.FormValue("email")
 		p := req.FormValue("password")
-		rp := req.FormValue("repassword")
 
 		// username token
 		if _, ok := dbUsers[un]; ok {
@@ -70,8 +74,13 @@ func signup(w http.ResponseWriter, req *http.Request) {
 		http.SetCookie(w, c)
 		dbSessions[c.Value] = un
 
-		//store user in database
-		u := user{un, e, p, rp}
+		// store user in dbUsers
+		bs, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.MinCost)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		u := user{un, e, bs}
 		dbUsers[un] = u
 
 		// Redirect
@@ -79,5 +88,45 @@ func signup(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 	tpl.ExecuteTemplate(w, "signup.html", nil)
+
+}
+
+func login(w http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	// process of form submission
+	if req.Method == http.MethodPost {
+		//get form values
+		un := req.FormValue("username")
+		p := req.FormValue("password")
+
+		//username token
+		u, ok := dbUsers[un]
+		if !ok {
+			http.Error(w, "username and/or password mismatch", http.StatusForbidden)
+			return
+		}
+
+		// Username and password matching
+		err := bcrypt.CompareHashAndPassword(u.Password, []byte(p))
+		if err != nil {
+			http.Error(w, "username and password invalid", http.StatusForbidden)
+			return
+		}
+
+		sID, _ := uuid.NewV4()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = un
+		http.Redirect(w, req, "/userPage", http.StatusSeeOther)
+		return
+	}
+	tpl.ExecuteTemplate(w, "login.html", nil)
 
 }
